585085e676679c61773e5e1b93edd616da5d99a6
adrioro <artistsvillage@me.com>
2017-04-20 09:25:22 -0400

    second commit

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,9 @@
-import media
+from . import media
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life","https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=NBepTulmSMw")
 
-print(toy_story.storyline)
+# print(toy_story.storyline)
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=a0CDJZu4M5I")
+
+print (avatar.storyline)
