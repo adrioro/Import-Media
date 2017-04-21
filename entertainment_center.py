c306f335c70e1ef77352d7d0043df1c8df5a93e3
adrioro <artistsvillage@me.com>
2017-04-21 17:21:03 -0400

    third commit

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,16 @@
-from . import media
+import media
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life","https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=NBepTulmSMw")
 
 # print(toy_story.storyline)
 
-avatar = media.Movie("Avatar", "A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=a0CDJZu4M5I")
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=KYk0zVOAOgQ&index=2&list=PLXZoqTfS5Cfb7ZHdqzJvKXCOtmhWRtt8D")
 
-print (avatar.storyline)
+#print (avatar.storyline)
+#avatar.show_trailer()
+
+redux = media.Movie ("Redux Riding Hood", "the familiar story of Red Riding Hood from the point of view of the big, bad wolf", "https://upload.wikimedia.org/wikipedia/en/9/9e/Redux_Riding_Hood_poster.jpg", "https://www.youtube.com/watch?v=dPDVdFS5p1g")
+
+print (redux.storyline)
+redux.show_trailer()
+
